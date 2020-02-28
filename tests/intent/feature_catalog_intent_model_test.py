3fa8dc552149edf7fe0037af9bf48203c07f986e
doatridge <darryl@opengrass.io>
2020-02-28 10:05:05 -0600

    - bug in discovery analyse_association

--- tests/intent/feature_catalog_intent_model_test.py
+++ tests/intent/feature_catalog_intent_model_test.py
@@ -5,13 +5,12 @@ import unittest
 import numpy as np
 import pandas as pd
 
-from ds_behavioral import DataBuilderTools as tools
+from ds_behavioral import SyntheticBuilder
 from ds_behavioral.sample.sample_data import ProfileSample
 
 from ds_discovery.intent.feature_catalog_intent import FeatureCatalogIntentModel
 from ds_discovery.managers.feature_catalog_property_manager import FeatureCatalogPropertyManager
 from aistac.handlers.abstract_handlers import ConnectorContract
-
 
 
 class FeatureCatalogIntentTest(unittest.TestCase):
@@ -21,8 +20,9 @@ class FeatureCatalogIntentTest(unittest.TestCase):
         property_manager = FeatureCatalogPropertyManager('test')
         property_manager.set_property_connector(ConnectorContract(uri='data/dummyfile.pickle', handler='DummyPersistHandler',
                                                                   module_name='aistac.handlers.dummy_handlers'))
-        property_manager.remove_intent()
-        self.clean = FeatureCatalogIntentModel(property_manager=property_manager)
+        property_manager.reset_intents()
+        self.intent = FeatureCatalogIntentModel(property_manager=property_manager)
+        self.tools = SyntheticBuilder.from_env('testing', default_save=False).intent_model
         try:
             os.makedirs('data')
         except:
@@ -38,13 +38,18 @@ class FeatureCatalogIntentTest(unittest.TestCase):
         """Basic smoke test"""
         FeatureCatalogIntentModel(property_manager=FeatureCatalogPropertyManager('test'), default_save_intent=False)
 
-    def test_auto_remove(self):
+    def test_apply_condition(self):
         df = pd.DataFrame()
-        df['gendre'] = tools.get_category( selection=['Comedy', 'Drama', 'News and Information', 'Reality and Game Show', 'Undefined'], size=20)
-        df['EndType'] = tools.get_category(selection=['Ad End', 'Ad Start', 'Undefined', 'Video End', 'Video Start'],
+        df['gendre'] = self.tools.get_category( selection=['Comedy', 'Drama', 'News and Information', 'Reality and Game Show', 'Undefined'], size=20)
+        df['EndType'] = self.tools.get_category(selection=['Ad End', 'Ad Start', 'Undefined', 'Video End', 'Video Start'],
                                            weight_pattern=[1, 3, 1, 6, 2], size=20)
-        result = self.clean.apply_condition(df, headers='EndType', condition='== value', value='Video End')
+        result = self.intent.apply_condition(df, headers='EndType', condition='== value', value='Video End')
         self.assertEqual(1, result['EndType'].nunique())
+
+    def test_group_features(self):
+        df = pd.DataFrame()
+        df['gendre'] = ['Comedy', 'Drama', 'Drama', 'Drama', 'Undefined']
+        df['spend'] = [1, 3, 2, 4, 20]
 
 
 
