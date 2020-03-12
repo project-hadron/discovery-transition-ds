a622478078a0b8be3c11f62fb80bf4ddc38f2a83
doatridge <darryl@opengrass.io>
2020-03-12 00:20:27 -0500

    - added visuals for confusion matrix - referenced behavioral intent for replace_missing

--- ds_discovery/intent/feature_catalog_intent.py
+++ ds_discovery/intent/feature_catalog_intent.py
@@ -14,6 +14,8 @@ from ds_discovery.intent.transition_intent import TransitionIntentModel
 from ds_discovery.transition.commons import Commons
 from ds_discovery.transition.discovery import DataDiscovery, DataAnalytics
 
+from ds_behavioral.intent.synthetic_intent_model import SyntheticIntentModel
+
 __author__ = 'Darryl Oatridge'
 
 
@@ -532,7 +534,7 @@ class FeatureCatalogIntentModel(AbstractIntentModel):
         return df_time.set_index(key)
 
     def replace_missing(self, canonical: pd.DataFrame, headers: [str, list], granularity: [int, float]=None,
-                        lower: [int, float]=None, upper: [int, float]=None, nulls_list: [bool, list]=None,
+                        lower: [int, float]=None, upper: [int, float]=None, nulls_list: list=None,
                         replace_zero: [int, float]=None, precision: int=None, day_first: bool=False,
                         year_first: bool=False, date_format: str = None, inplace: bool=False, save_intent: bool=None,
                         intent_level: [int, str]=None) -> [None, pd.DataFrame]:
@@ -546,7 +548,7 @@ class FeatureCatalogIntentModel(AbstractIntentModel):
                 pd.Timedelta passed - a frequency time delta
         :param lower: (optional) the lower limit of the number or date value. Takes min() if not set
         :param upper: (optional) the upper limit of the number or date value. Takes max() if not set
-        :param nulls_list: (optional) a list of nulls other than np.nan
+        :param nulls_list: (optional) a list of nulls other than np.nan that should be considered null
         :param replace_zero: (optional) if zero what to replace the weighting value with to avoid zero probability
         :param precision: (optional) by default set to 3.
         :param day_first: if the date provided has day first
@@ -561,40 +563,39 @@ class FeatureCatalogIntentModel(AbstractIntentModel):
         self._set_intend_signature(self._intent_builder(method=inspect.currentframe().f_code.co_name, params=locals()),
                                    intent_level=intent_level, save_intent=save_intent)
         # intend code block on the canonical
+        sim = SyntheticIntentModel(self._pm, default_save_intent=False)
         if not inplace:
             with threading.Lock():
                 canonical = deepcopy(canonical)
         headers = self._pm.list_formatter(headers)
         if not isinstance(canonical, pd.DataFrame):
             raise TypeError("The canonical given is not a pandas DataFrame")
-        if isinstance(nulls_list, bool) and nulls_list:
-            nulls_list = ['NaN', 'nan', 'null', '', 'None', np.inf, -np.inf]
-        elif not isinstance(nulls_list, list):
-            nulls_list = None
+        nulls_list = nulls_list if isinstance(nulls_list, list) else ['nan', '']
         for c in headers:
             col = deepcopy(canonical[c])
             # replace alternative nulls with pd.nan
             if nulls_list is not None:
-                col.replace(nulls_list, np.nan, inplace=True)
+                for null in self._pm.list_formatter(nulls_list):
+                    col.replace(null, np.nan, inplace=True)
             size = len(col[col.isna()])
             if size > 0:
                 if is_numeric_dtype(col):
                     result = DataDiscovery.analyse_number(col, granularity=granularity, lower=lower, upper=upper,
                                                           precision=precision)
-                    col[col.isna()] = self._get_number(from_value=result.get('lower'), to_value=result.get('upper'),
-                                                       weight_pattern=result.get('weighting'), precision=0, size=size)
+                    col[col.isna()] = sim.get_number(from_value=result.get('lower'), to_value=result.get('upper'),
+                                                     weight_pattern=result.get('weighting'), precision=0, size=size)
                 elif is_datetime64_any_dtype(col):
                     result = DataDiscovery.analyse_date(col, granularity=granularity, lower=lower, upper=upper,
                                                         day_first=day_first, year_first=year_first,
                                                         date_format=date_format)
-                    synthetic = self._get_datetime(start=result.get('lower'), until=result.get('upper'),
-                                                   weight_pattern=result.get('weighting'), date_format=date_format,
-                                                   day_first=day_first, year_first=year_first, size=size)
+                    synthetic = sim.get_datetime(start=result.get('lower'), until=result.get('upper'),
+                                                 weight_pattern=result.get('weighting'), date_format=date_format,
+                                                 day_first=day_first, year_first=year_first, size=size)
                     col = col.apply(lambda x: synthetic.pop() if x is pd.NaT else x)
                 else:
                     result = DataDiscovery.analyse_category(col, replace_zero=replace_zero)
-                    col[col.isna()] = self._get_category(selection=result.get('selection'),
-                                                         weight_pattern=result.get('weighting'), size=size)
+                    col[col.isna()] = sim.get_category(selection=result.get('selection'),
+                                                       weight_pattern=result.get('weighting'), size=size)
             canonical[c] = col
         if inplace:
             return
@@ -663,219 +664,3 @@ class FeatureCatalogIntentModel(AbstractIntentModel):
     """
         PRIVATE METHODS SECTION
     """
-    def _get_number(self, from_value: [int, float], to_value: [int, float]=None, weight_pattern: list=None,
-                    offset: int=None, precision: int=None, bounded_weighting: bool=True, at_most: int=None,
-                    dominant_values: [float, list]=None, dominant_percent: float=None, dominance_weighting: list=None,
-                    size: int = None) -> list:
-        """ returns a number in the range from_value to to_value. if only to_value given from_value is zero
-
-        :param from_value: range from_value to_value if to_value is used else from 0 to from_value if to_value is None
-        :param to_value: optional, (signed) integer to end from.
-        :param weight_pattern: a weighting pattern or probability that does not have to add to 1
-        :param precision: the precision of the returned number. if None then assumes int value else float
-        :param offset: an offset multiplier, if None then assume 1
-        :param bounded_weighting: if the weighting pattern should have a soft or hard boundary constraint
-        :param at_most: the most times a selection should be chosen
-        :param dominant_values: a value or list of values with dominant_percent. if used MUST provide a dominant_percent
-        :param dominant_percent: a value between 0 and 1 representing the dominant_percent of the dominant value(s)
-        :param dominance_weighting: a weighting of the dominant values
-        :param size: the size of the sample
-        :return: a random number
-        """
-        (from_value, to_value) = (0, from_value) if not isinstance(to_value, (float, int)) else (from_value, to_value)
-        at_most = 0 if not isinstance(at_most, int) else at_most
-        if at_most > 0 and (at_most * (to_value-from_value)) < size:
-            raise ValueError("When using 'at_most', the selectable values must be greater than the size. selectable "
-                             "value count is '{}',size requested is '{}'".format(at_most * (to_value-from_value), size))
-        size = 1 if size is None else size
-        offset = 1 if offset is None else offset
-        dominant_percent = 0 if not isinstance(dominant_percent, (int, float)) else dominant_percent
-        dominant_percent = dominant_percent / 100 if 1 < dominant_percent <= 100 else dominant_percent
-        _limit = 10000
-        precision = 3 if not isinstance(precision, int) else precision
-        if precision == 0:
-            from_value = int(round(from_value, 0))
-            to_value = int(round(to_value, 0))
-        is_int = True if isinstance(to_value, int) and isinstance(from_value, int) else False
-        if is_int:
-            precision = 0
-        dominant_list = []
-        if isinstance(dominant_values, (int, float, list)):
-            sample_count = int(round(size * dominant_percent, 1)) if size > 1 else 0
-            dominance_weighting = [1] if not isinstance(dominance_weighting, list) else dominance_weighting
-            if sample_count > 0:
-                if isinstance(dominant_values, list):
-                    dominant_list = self._get_category(selection=dominant_values, weight_pattern=dominance_weighting,
-                                                       size=sample_count, bounded_weighting=True)
-                else:
-                    dominant_list = [dominant_values] * sample_count
-            size -= sample_count
-        if weight_pattern is not None:
-            counter = [0] * len(weight_pattern)
-            if bounded_weighting:
-                unit = size/sum(weight_pattern)
-                for i in range(len(weight_pattern)):
-                    counter[i] = int(round(weight_pattern[i] * unit, 0))
-                    if 0 < at_most < counter[i]:
-                        counter[i] = at_most
-                    if counter[i] == 0 and weight_pattern[i] > 0:
-                        if counter[self._weighted_choice(weight_pattern)] == i:
-                            counter[i] = 1
-            else:
-                for _ in range(size):
-                    counter[self._weighted_choice(weight_pattern)] += 1
-                for i in range(len(counter)):
-                    if 0 < at_most < counter[i]:
-                        counter[i] = at_most
-            while sum(counter) != size:
-                if at_most > 0:
-                    for index in range(len(counter)):
-                        if counter[index] >= at_most:
-                            counter[index] = at_most
-                            weight_pattern[index] = 0
-                if sum(counter) < size:
-                    counter[self._weighted_choice(weight_pattern)] += 1
-                else:
-                    weight_idx = self._weighted_choice(weight_pattern)
-                    if counter[weight_idx] > 0:
-                        counter[weight_idx] -= 1
-
-        else:
-            counter = [size]
-        rtn_list = []
-        if is_int:
-            value_bins = []
-            ref = from_value
-            counter_len = len(counter)
-            select_len = to_value - from_value
-            for index in range(1, counter_len):
-                position = int(round(select_len / counter_len * index, 1)) + from_value
-                value_bins.append((ref, position))
-                ref = position
-            value_bins.append((ref, to_value))
-            for index in range(counter_len):
-                low, high = value_bins[index]
-                if low >= high:
-                    rtn_list += [low] * counter[index]
-                elif at_most > 0:
-                    choice = []
-                    for _ in range(at_most):
-                        choice += list(range(low, high))
-                    np.random.shuffle(choice)
-                    rtn_list += [int(np.round(value, precision)) for value in choice[:counter[index]]]
-                else:
-                    _remaining = counter[index]
-                    while _remaining > 0:
-                        _size = _limit if _remaining > _limit else _remaining
-                        rtn_list += np.random.randint(low=low, high=high, size=_size).tolist()
-                        _remaining -= _limit
-        else:
-            value_bins = pd.interval_range(start=from_value, end=to_value, periods=len(counter), closed='both')
-            for index in range(len(counter)):
-                low = value_bins[index].left
-                high = value_bins[index].right
-                if low >= high:
-                    rtn_list += [low] * counter[index]
-                elif at_most > 0:
-                    choice = []
-                    for _ in range(at_most):
-                        choice += list(range(low, high))
-                    np.random.shuffle(choice)
-                    rtn_list += [np.round(value, precision) for value in choice[:counter[index]]]
-                else:
-                    _remaining = counter[index]
-                    while _remaining > 0:
-                        _size = _limit if _remaining > _limit else _remaining
-                        rtn_list += np.round((np.random.random(size=_size)*(high-low)+low), precision).tolist()
-                        _remaining -= _limit
-        if offset != 1:
-            rtn_list = [value*offset for value in rtn_list]
-        # add in the dominant values
-        rtn_list = rtn_list + dominant_list
-        np.random.shuffle(rtn_list)
-        return rtn_list
-
-    def _get_category(self, selection: list, weight_pattern: list=None, size: int=None, at_most: int=None,
-                      bounded_weighting: bool=None) -> list:
-        """ returns a category from a list. Of particular not is the at_least parameter that allows you to
-        control the number of times a selection can be chosen.
-
-        :param selection: a list of items to select from
-        :param weight_pattern: a weighting pattern that does not have to add to 1
-        :param size: an optional size of the return. default to 1
-        :param at_most: the most times a selection should be chosen
-        :param bounded_weighting: if the weighting pattern should have a soft or hard boundary (default False)
-        :return: an item or list of items chosen from the list
-        """
-        if not isinstance(selection, list) or len(selection) == 0:
-            return [None]*size
-        bounded_weighting = False if not isinstance(bounded_weighting, bool) else bounded_weighting
-        select_index = self._get_number(len(selection), weight_pattern=weight_pattern, at_most=at_most,
-                                        size=size, bounded_weighting=bounded_weighting)
-        rtn_list = [selection[i] for i in select_index]
-        return rtn_list
-
-    def _get_datetime(self, start, until, weight_pattern: list=None, at_most: int=None, date_format: str=None,
-                      as_num: bool=False, ignore_time: bool=False, size: int=None,
-                      day_first: bool=False, year_first: bool=False) -> list:
-        """ returns a random date between two date and times. weighted patterns can be applied to the overall date
-        range, the year, month, day-of-week, hours and minutes to create a fully customised random set of dates.
-        Note: If no patterns are set this will return a linearly random number between the range boundaries.
-              Also if no patterns are set and a default date is given, that default date will be returnd each time
-
-        :param start: the start boundary of the date range can be str, datetime, pd.datetime, pd.Timestamp
-        :param until: then up until boundary of the date range can be str, datetime, pd.datetime, pd.Timestamp
-        :param weight_pattern: (optional) A pattern across the whole date range.
-        :param at_most: the most times a selection should be chosen
-        :param ignore_time: ignore time elements and only select from Year, Month, Day elements. Default is False
-        :param date_format: the string format of the date to be returned. if not set then pd.Timestamp returned
-        :param as_num: returns a list of Matplotlib date values as a float. Default is False
-        :param size: the size of the sample to return. Default to 1
-        :param year_first: specifies if to parse with the year first
-                If True parses dates with the year first, eg 10/11/12 is parsed as 2010-11-12.
-                If both dayfirst and yearfirst are True, yearfirst is preceded (same as dateutil).
-        :param day_first: specifies if to parse with the day first
-                If True, parses dates with the day first, eg %d-%m-%Y.
-                If False default to the a prefered preference, normally %m-%d-%Y (but not strict)
-        :return: a date or size of dates in the format given.
-         """
-        as_num = False if not isinstance(as_num, bool) else as_num
-        ignore_time = False if not isinstance(ignore_time, bool) else ignore_time
-        if start is None or until is None:
-            raise ValueError("The start or until parameters cannot be of NoneType")
-        size = 1 if size is None else size
-        _dt_start = self._convert_date2value(start, day_first=day_first, year_first=year_first)[0]
-        _dt_until = self._convert_date2value(until, day_first=day_first, year_first=year_first)[0]
-        precision = 15
-        if ignore_time:
-            _dt_start = int(_dt_start)
-            _dt_until = int(_dt_until)
-            precision = 0
-        rtn_list = self._get_number(from_value=_dt_start, to_value=_dt_until, weight_pattern=weight_pattern,
-                                    at_most=at_most, precision=precision, size=size)
-        if not as_num:
-            rtn_list = mdates.num2date(rtn_list)
-            if isinstance(date_format, str):
-                rtn_list = pd.Series(data=rtn_list).dt.strftime(date_format).tolist()
-        return rtn_list
-
-    @staticmethod
-    def _weighted_choice(weights: list):
-        """ a probability weighting based on the values in the integer list
-
-        :param weights: a list of integers representing a pattern of weighting
-        :return: an index of which weight was randomly chosen
-        """
-        if not isinstance(weights, list) or not all(isinstance(x, (int, float, list)) for x in weights):
-            raise ValueError("The weighted pattern must be an list of integers")
-        rnd = np.random.random() * sum(weights)
-        for i, w in enumerate(weights):
-            rnd -= w
-            if rnd < 0:
-                return i
-
-    @staticmethod
-    def _convert_date2value(dates, day_first: bool = True, year_first: bool = False):
-        values = pd.to_datetime(dates, errors='coerce', infer_datetime_format=True, dayfirst=day_first,
-                                yearfirst=year_first)
-        return mdates.date2num(pd.Series(values)).tolist()
