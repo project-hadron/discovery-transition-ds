7159d9aa9fd4cb8e3064a19f70d85543af488508
doatridge <darryl@opengrass.io>
2020-04-26 15:03:41 -0500

    - change version init file to include Transition and Feature Catalog imports

--- ds_discovery/__init__.py
+++ ds_discovery/__init__.py
@@ -1,5 +1,6 @@
 # bring definitions to the top level
 from ds_discovery.transition.transitioning import Transition
+from ds_discovery.transition.feature_catalog import FeatureCatalog
 
 # release version number picked up in the setup.py
-__version__ = "2.08.002"
+__version__ = "2.08.003"
