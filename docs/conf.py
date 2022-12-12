34dedee5fedb5ab7b5d3ee6de6fbc16f16dae5ed
gigas64 <gigas64@opengrass.net>
2022-12-12 13:10:59 -0600

    - mock ds_discovery in docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
-from unittest import mock
-
 sys.path.insert(0, os.path.abspath('../'))
-
-# Mock ds_discovery because it fails to build in readthedocs
-MOCK_MODULES = ["ds_discovery.component.transitioning"]
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information
 project = 'Project Hadron'
